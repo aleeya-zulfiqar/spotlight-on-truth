49aaac825c92c51d9e42b7ba00a88348a55a7b00
aleeya-zulfiqar <l226791@lhr.nu.edu.pk>
2025-12-02 16:31:30 +0500

    Feature: setup frontend (Vite + React + Tailwind)

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="text-xl text-white bg-black min-h-screen flex items-center justify-center">
+      <h1>Spotlight On Truth</h1>
+    </div>
+  )
+}
+
+export default App
